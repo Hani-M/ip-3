5b8beeeed7b41ef4aae24a9c213b94aacfa79942
Hani-M <hani@saifproperties.com>
2020-07-27 12:31:17 +0300

    added the js submit

--- index.js
+++ index.js
@@ -78,3 +78,17 @@ $(document).ready(function(){
         $("#overlay8").hide();
       });
     });
+    $(document).ready(function(){
+        $("form#form34A").submit(function(event){
+          // event.preventDefault();
+          var name = $("input#MERGE1").val();
+          var email = $("input#MERGE0").val();
+          var message = $("textarea#comment").val();
+          if ($(email).val() && $(message).val()){
+            alert (name + ", we have received your message. Thank you for reaching out to us.");
+          }
+          else {
+            alert("Please enter your name and email!");
+          }
+        });
+      });
