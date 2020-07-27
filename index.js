369201d60ac75f352f0b43cc4a2d2b2097ea34e3
Hani-M <hani@saifproperties.com>
2020-07-27 12:26:47 +0300

    added the wokr 7 8 function

--- index.js
+++ index.js
@@ -56,3 +56,25 @@ $(document).ready(function(){
       $("#overlay4").hide();
     });
   });
+  $(document).ready(function(){
+    $("#work5").mouseover(function(){
+      $("#overlay5").show();
+    }).mouseout(function(){
+      $("#overlay5").hide();
+    });
+    $("#work6").mouseover(function(){
+      $("#overlay6").show();
+    }).mouseout(function(){
+      $("#overlay6").hide();
+    });
+    $("#work7").mouseover(function(){
+      $("#overlay7").show();
+    }).mouseout(function(){
+      $("#overlay7").hide();
+    }); 
+    $("#work8").mouseover(function(){
+        $("#overlay8").show();
+      }).mouseout(function(){
+        $("#overlay8").hide();
+      });
+    });
